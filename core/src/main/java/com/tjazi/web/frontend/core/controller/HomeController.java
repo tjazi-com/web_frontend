26ad70fd005ce7fec67b6df275dc2dbc631b3a4b
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2016-03-17 21:23:17 +0000

    Adding sample mock reference (via profile setting)

--- core/src/main/java/com/tjazi/web/frontend/core/controller/HomeController.java
+++ core/src/main/java/com/tjazi/web/frontend/core/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.tjazi.web.frontend.core.controller;
 
+import com.tjazi.chatrooms.client.ChatroomsClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.UUID;
 
 /**
  * Created by Krzysztof Wasiak on 08/03/2016.
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ChatroomsClient chatroomsClient;
+
     @RequestMapping(value = {"/", "index", "index.html"}, method = RequestMethod.GET)
     public String index() {
+
+        chatroomsClient.deleteChatroom(UUID.randomUUID());
         return "index";
     }
 }
